47a5fdfe66a72a0db378a760eb5cbe8a937bbb4a
Tim Kormachev <wardruid@yandex.ru>
2019-05-30 07:16:49 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from resources.user import UserRegister
+from security import authenticate, identity
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///D:\git\flask_sqlalchemy_tutorial\data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "tim"
+api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    from db import db
+
+    db.init_app(app)
+    app.run(port=5000, debug=True)
